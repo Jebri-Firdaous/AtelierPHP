71e64ac4fe1076ed1b1c80dd2febe1550d3ce214
Firdaous <56646201+Jebri-Firdaous@users.noreply.github.com>
2023-09-18 05:42:46 +0100

    partie4_app

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+class Config {
+    private static $instance = NULL;
+
+    public static function getConnexion() {
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=clubEsprit', 'root', '');
+        }
+        return self::$instance;
+    }
+}
+?>
