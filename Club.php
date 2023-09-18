b6052f45f9dc8504a6d92b90c985c9022a6a53b8
Firdaous <56646201+Jebri-Firdaous@users.noreply.github.com>
2023-09-18 05:56:43 +0100

    final commit

--- Club.php
+++ Club.php
@@ -1,36 +1,51 @@
 <?php
-class Club {
+class Club 
+{
     private $id;
     private $nom;
     private $description;
     private $adresse;
     private $domaine;
 
-    public function __construct($id, $nom, $description, $adresse, $domaine) {
+    public function __construct($id, $nom, $description, $adresse, $domaine)
+     {
         $this->id = $id;
         $this->nom = $nom;
         $this->description = $description;
         $this->adresse = $adresse;
         $this->domaine = $domaine;
     }
+    public function afficherClub() 
+    {
+        echo "ID: " . $this->id . "<br>";
+        echo "Nom: " . $this->nom . "<br>";
+        echo "Description: " . $this->description . "<br>";
+        echo "Adresse: " . $this->adresse . "<br>";
+        echo "Domaine: " . $this->domaine . "<br>";
+    }
 
- 
-    public static function afficherClubs() {
+    public static function afficherClubs()
+     {
         $connexion = Config::getConnexion();
         $query = "SELECT * FROM club";
         $result = $connexion->query($query);
     
-        if ($result) {
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        if ($result) 
+        {
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) 
+            {
                 echo "ID : " . $row['id'] . "<br>";
                 echo "Nom : " . $row['nom'] . "<br>";
                 echo "Description : " . $row['description'] . "<br>";
                 echo "Adresse : " . $row['adresse'] . "<br>";
                 echo "Domaine : " . $row['domaine'] . "<br><br>";
             }
-        } else {
+        } else 
+        {
             echo "Erreur lors de la récupération des clubs.";
         }
+    
+        
     }
 }
 ?>
